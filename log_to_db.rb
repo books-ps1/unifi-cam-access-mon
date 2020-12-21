7dca85e609abf99b9c17a5409c861fcbd874a296
Books <books@pumpingstationone.org>
2020-12-21 11:38:47 +0000

    Create log_to_db.rb script to insert the access lines of a log into a DB

---
+++ log_to_db.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+#
+require 'dotenv/load'
+require_relative './lib/access_line'
+
+log_file_path = ARGV[0]
+
+# The database must already exist for this script to work.
+# Create or migrate the database with migrate.rb
+ActiveRecord::Base.establish_connection(
+  :adapter  => 'postgresql',
+  :encoding => 'unicode',
+  :database => ENV['DB_NAME'], # accessmon
+  :username => ENV['DB_USERNAME'], # accessmon
+  :password => ENV['DB_PASSWORD'],
+  :pool     => 5,
+  :port     => 5432,
+  :host     => 'localhost')
+
+File.new(log_file_path, 'r').each_line do |line|
+  match = AccessLine.match?(line)
+  if match
+    timestamp, username, peer_id = match
+    AccessLine.create(line: line,
+                      username: username,
+                      timestamp: timestamp,
+                      peer_id: peer_id)
+  end
+end
